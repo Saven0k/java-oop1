a1acdd5c2d40027349a8710373fc8d5882330f23
Savenok <Romasav2017@gmail.com>
2023-02-17 15:57:40 +0300

    hotdrinkve

---
+++ HotDrinkVendingMachine.java
@@ -0,0 +1,18 @@
+﻿import java.util.ArrayList;
+public class HotDrinkVendingMachine  extends VendingMachine{
+
+    private ArrayList<HotDrink>  good;
+
+    public HotDrinkVendingMachine(ArrayList<HotDrink>  goods){
+        this.good = goods;
+    }
+    public HotDrink getProduct(String name, double volume, double temperature){
+        for (HotDrink el: this.good) {
+            if (HotDrink instance of HotDrink){
+                HotDrink ht = (HotDrink) product; 
+            }
+        }
+        return null;
+    }
+    
+}
