7f269fa5d5e153ad1c74cc602465e2924b2a31b9
Savenok <Romasav2017@gmail.com>
2023-02-17 15:57:15 +0300

    drinkmachine

---
+++ DrinkMachine.java
@@ -0,0 +1,21 @@
+﻿import java.util.ArrayList;
+public class DrinkMachine extends VendingMachine{
+    private ArrayList<Drink> drinkList;
+
+    public DrinkMachine(ArrayList<Product> goods){
+        super(goods);
+        drinkList = new ArrayList<Drink>();
+    }
+
+    public void addDrink(Drink dr){
+        drinkList.add(dr);
+    }
+    public Product getProductByName(String name){
+        for (Drink el: drinkList) {
+            if(el.getName().equals(name)) return el;
+        }
+        return null;
+    }
+
+
+}
