614550d50245493db4d924d8d487c450191c95ce
Savenok <Romasav2017@gmail.com>
2023-02-17 15:56:48 +0300

    main

---
+++ main.java
@@ -0,0 +1,22 @@
+﻿import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+        List<Product> products = Arrays.asList(
+            new Product("Apple", 3),
+            new Drink("cocacola", 5),
+            new Product("cheese", 7),
+            new Drink("shveps", 9),
+            new HotDrink("tea", 5, 30),
+            new HotDrink("Coffi", 7, 55));
+
+        for (Product pr : products) {
+            pr.consume();
+        }
+        
+        
+        
+    }
+}
