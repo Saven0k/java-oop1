40674d3ed45a5707ac7cc204069c85fb5c91fb29
Savenok <Romasav2017@gmail.com>
2023-02-17 15:58:01 +0300

    venmach

---
+++ VendingMachine.java
@@ -0,0 +1,22 @@
+﻿import java.util.ArrayList;
+public class VendingMachine {
+    private ArrayList<Product>  goods;
+
+    public VendingMachine(ArrayList<Product>  goods){
+        this.goods = goods;
+    }
+
+    @Override
+    public String toString(){   
+        String g = "";
+        return g;
+    }
+    public Product getProductByName(String name){
+        for (Product el: goods) {
+            if(el.getName().equals(name)) return el;
+        }
+        return null;
+    }
+
+    
+}
