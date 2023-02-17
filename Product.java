12ebb4ddf8f83b702e910071ad391d732693a3ab
Savenok <Romasav2017@gmail.com>
2023-02-17 15:57:51 +0300

    product

---
+++ Product.java
@@ -0,0 +1,30 @@
+﻿public class Product {
+    private String name;
+    private float price;
+
+    public Product(String name, float price){
+        this.name = name;
+        this.price= price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public float getPrice(){
+        return price;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPrice(float price){
+
+        this.price = price;
+    }
+    
+    public void consume(){
+        System.out.println("Продукт потреблен");
+    }
+}
