cc927ec8a0fa15a6985560ecc0c22d3401a97665
Savenok <Romasav2017@gmail.com>
2023-02-17 15:56:59 +0300

    Drink

---
+++ Drink.java
@@ -0,0 +1,13 @@
+﻿public class Drink extends Product{
+    private int volume;
+
+    Drink(String name, float price){
+        super(name, price);
+    }
+    public void setVolume(int volume){
+        this.volume = volume;
+    }
+    public void consume(){
+        System.out.println("Напиток выпит");
+    };
+}
